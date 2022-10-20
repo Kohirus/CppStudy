290428343f42c1d26614f3ef304bb231ae612136
tuilk <likh.aini@outlook.com>
2022-10-20 17:56:30 +0800

    add singleton pattern

--- DesignPatterns/Singleton.cpp
+++ DesignPatterns/Singleton.cpp
@@ -1,3 +1,12 @@
 #include "Singleton.h"
 
-Singleton::_instance = nullptr;
+Singleton* Singleton::_instance = nullptr;
+
+EagerSingleton* EagerSingleton::_instance = new Singleton();
+
+LazySingleton* LazySingleton::_instance = nullptr;
+#if defined(_WIN32) || defined(_WIN64)
+std::mutex LazySingleton::_mutex;
+#elif __linux__
+pthread_mutex_t LazySingleton::_mutex = PTHREAD_MUTEX_INITIALIZER;
+#endif
--- DesignPatterns/Singleton.h
+++ DesignPatterns/Singleton.h
@@ -1,6 +1,19 @@
 #ifndef _SINGLETON_H_
 #define _SINGLETON_H_
 
+#if defined(_WIN32) || defined(_WIN64)
+
+#include <mutex>
+typedef std::mutex Mutex;
+
+#elif __linux__
+
+#include <pthread_mutex.h>
+typedef pthread_mutex_t Mutex;
+
+#endif
+
+/// @brief 普通单例模式
 class Singleton {
 public:
     static Singleton* getInstance() {
@@ -13,6 +26,48 @@ private:
     }
 
     static Singleton* _instance;
+};
+
+/// @brief 饿汉式单例类
+class EagerSingleton {
+public:
+    static EagerSingleton* getInstance() { return _instance; }
+
+private:
+    EagerSingleton();
+    static EagerSingleton* _instance;
+};
+
+/// @brief 懒汉式单例类 通过互斥锁确保线程安全 使用双检锁机制
+class LazySingleton {
+public:
+    static LazySingleton* getInstance() {
+        if (_instance == nullptr) {
+            _mutex.lock();
+            if (_instance == nullptr) {
+                _instance = new LazySingleton();
+            }
+            _mutex.unlock();
+        }
+        return _instance;
+    }
+
+private:
+    LazySingleton();
+    static LazySingleton* _instance;
+    static Mutex          _mutex;
+};
+
+/// @brief IoDH单例类
+class IoDHSingleton {
+public:
+    static IoDHSingleton* getInstance() {
+        static IoDHSingleton instance;
+        return &instance;
+    }
+
+private:
+    IoDHSingleton();
 };
 
 #endif // !_SINGLETON_H_
